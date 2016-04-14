8bdb79f507d4c93e45e9fa9d74aa27abbf8b5bb0
Admiral Potato <admiral@nuclearpixel.com>
2016-04-14 02:52:37 -0600

    Adding a basic static server

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static('public'));
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
