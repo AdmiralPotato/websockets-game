ee042da94d5af34003e05825cc235c7a921730ea
Admiral Potato <admiral@nuclearpixel.com>
2016-04-24 20:24:37 -0600

    Joystick display is looking and working a bit better

--- public/play.js
+++ public/play.js
@@ -16,22 +16,29 @@ if(split.length === 1){
 	);
 	var cursor = { x: 0, y: 0 };
 	var joystickInfluence = pi / 8;
+	var joystockHolder = new n.Ob3D({shape: {}});
+	joystockHolder.update = function() {
+		var gb = gameBoardHolder;
+		this.pos[gb.smallerAxis] = 0;
+		this.pos[gb.offsetAxis] = gb.min;
+		n.Maths.pointAt(this, scene.camera.pos);
+	};
+	scene.add(joystockHolder);
 	var joystick = new n.Ob3D({
 		shape: shapes.joystick,
-		color: '#999'
+		color: '#999',
+		rotOrder: [2,1,0]
 	});
 	joystick.update = function(){
 		var gb = gameBoardHolder;
 		var scale = gb.min / 2;
-		joystick.scale[0] = scale;
-		joystick.scale[1] = scale;
-		joystick.scale[2] = scale;
-		joystick.pos[gb.smallerAxis] = 0;
-		joystick.pos[gb.offsetAxis] = gb.min;
-		joystick.rot[0] = cursor.y * -joystickInfluence;
-		joystick.rot[1] = cursor.x * joystickInfluence;
+		this.scale[0] = scale;
+		this.scale[1] = scale;
+		this.scale[2] = scale;
+		this.rot[0] = cursor.x * -joystickInfluence;
+		this.rot[1] = cursor.y * -joystickInfluence + (pi / 2);
 	};
-	scene.add(joystick);
+	joystockHolder.add(joystick);
 	var isActive = false;
 	var onOffHandler = function(e){
 		isActive = 'mousedown keydown touchstart'.indexOf(e.type) !== -1;
@@ -40,10 +47,16 @@ if(split.length === 1){
 	var moveHandler = function(e){
 		var gb = gameBoardHolder;
 		var scale = gb.scale[0]
-		var xOffset = !gb.offsetAxis ? gb.min: 0;
-		var yOffset = gb.min - xOffset;
-		cursor.x = (scene.mpos.x - xOffset) / scale * 2;
-		cursor.y = (scene.mpos.y - yOffset) / scale * 2;
+		var xOffset = gb.offsetAxis === 0 ? gb.min : 0;
+		var yOffset = gb.offsetAxis === 1 ? gb.min : 0;
+		var x = (scene.mpos.x - xOffset) / scale * 2;
+		var y = (scene.mpos.y - yOffset) / scale * 2;
+		var angle = Math.atan2(y, x);
+		var lengthMax = 1;
+		var length = Math.sqrt(x*x + y*y);
+		var correctedLength = Math.min(length, lengthMax);
+		cursor.x = cos(angle) * correctedLength;
+		cursor.y = sin(angle) * correctedLength;
 	};
 	var cursorEmitter = function(){
 		if(isActive){
